6473c46e6d61b105813b5fd3932ae7cd5e57b817
triastanto <rahmadhany.triastanto@gmail.com>
2023-08-14 14:49:41 +0700

    modify user migration & create migration

--- database/migrations/2023_08_14_074314_create_submissions_table.php
+++ database/migrations/2023_08_14_074314_create_submissions_table.php
@@ -11,14 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('submissions', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->enum('role', ['Administrator', 'Submitter', 'Approver']);
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->foreignId('user_id')->constrained(); // users
+            $table->foreignId('status_id')->constrained(); // statuses
+            $table->enum('type', ['Leave', 'Permit', 'Sick']);
+            $table->string('attachment');
             $table->timestamps();
         });
     }
@@ -28,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('submissions');
     }
 };
--- database/migrations/2023_08_14_074317_create_histories_table.php
+++ database/migrations/2023_08_14_074317_create_histories_table.php
@@ -11,14 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('histories', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->enum('role', ['Administrator', 'Submitter', 'Approver']);
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->foreignId('submission_id')->constrained(); // submissions
+            $table->foreignId('user_id')->constrained(); // users
+            $table->foreignId('status_id')->constrained(); // statuses
+            $table->text('comments');
             $table->timestamps();
         });
     }
@@ -28,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('histories');
     }
 };
