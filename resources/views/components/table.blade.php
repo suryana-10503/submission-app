12566b1931ed1ff548f6030ead665b2d8d5abf4b
suryana-10503 <suryana.10503@gmail.com>
2023-08-15 14:25:56 +0700

    create view components

---
+++ resources/views/components/table.blade.php
@@ -0,0 +1,80 @@
+<div class="block w-full overflow-x-auto">
+    <table class="items-center bg-transparent w-full border-collapse">
+        <thead>
+            <tr>
+                <th
+                    class="px-6 py-3 text-xs font-semibold text-left bg-blueGray-50 text-blueGray-500 border border-blueGray-100 whitespace-nowrap border-l-0 border-r-0 uppercase">
+                    ID
+                </th>
+                <th
+                    class="px-6 py-3 text-xs font-semibold text-left bg-blueGray-50 text-blueGray-500 border border-blueGray-100 whitespace-nowrap border-l-0 border-r-0 uppercase">
+                    Date
+                </th>
+                <th
+                    class="px-6 py-3 text-xs font-semibold text-left bg-blueGray-50 text-blueGray-500 border border-blueGray-100 whitespace-nowrap border-l-0 border-r-0 uppercase">
+                    Submitter
+                </th>
+                <th
+                    class="px-6 py-3 text-xs font-semibold text-left bg-blueGray-50 text-blueGray-500 border border-blueGray-100 whitespace-nowrap border-l-0 border-r-0 uppercase">
+                    Status
+                </th>
+            </tr>
+        </thead>
+  
+        <tbody>
+            <tr>
+                <th
+                    class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left text-blueGray-700 ">
+                    1
+                </th>
+                <td
+                    class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 ">
+                    2023-08-10
+                </td>
+                <td
+                    class="border-t-0 px-6 align-center border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                    UserA
+                </td>
+                <td
+                    class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                    <p class="text-gray-600">Pending</p>
+                </td>
+            </tr>
+            <tr>
+                <th
+                    class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left text-blueGray-700">
+                    2
+                </th>
+                <td
+                    class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                    2023-08-12
+                </td>
+                <td
+                    class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                    UserB
+                </td>
+                <td
+                    class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                    <p class="text-red-600">Rejected</p>
+                </td>
+            </tr>
+            <tr>
+                <th
+                    class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left text-blueGray-700">
+                    3
+                </th>
+                <td
+                    class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                    2023-07-05
+                </td>
+                <td
+                    class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                    UserC
+                </td>
+                <td
+                    class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                    <p class="text-green-600">Approved</p>
+                </td>
+        </tbody>
+    </table>
+  </div>
