9f5e2daca7ef127d987642c4531c29ba5d70e160
suryana-10503 <suryana.10503@gmail.com>
2023-08-16 10:35:23 +0700

    session page, store and add & moddleware

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -3,8 +3,18 @@
         <div class="container mx-auto flex justify-between items-center">
             <a href="#" class="text-white font-semibold text-lg">{{ config('app.name') }}</a>
             <ul class="flex space-x-4">
-                <li><a href="register" class="text-white hover:text-gray-400">Register</a></li>
-                <li><a href="login" class="text-white hover:text-gray-400">Login</a></li>
+                @auth
+                    <li><a href="register" class="text-white hover:text-gray-400">Home</a></li>
+                    <form action="logout" method="POST">
+                      @csrf
+                      <button type="submit" class="text-white hover:text-gray-400 focus:outline-none">
+                          Logout
+                      </button>
+                  </form>
+                @else
+                    <li><a href="register" class="text-white hover:text-gray-400">Register</a></li>
+                    <li><a href="login" class="text-white hover:text-gray-400">Login</a></li>
+                @endauth
             </ul>
         </div>
     </nav>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\SessionController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,10 +16,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::get('/', [HomeController::class, 'welcome']);
-Route::get('home', [HomeController::class, 'index']);
+Route::get('home', [HomeController::class, 'index'])->middleware('auth');
 
-Route::get('register', [RegisterController::class, 'create']);
-Route::post('register', [RegisterController::class, 'store']);
+Route::get('register', [RegisterController::class, 'create'])->middleware('guest');
+Route::post('register', [RegisterController::class, 'store'])->middleware('guest');
+Route::get('login', [SessionController::class, 'create'])->middleware('guest')->name('login');
+Route::post('login', [SessionController::class, 'store'])->middleware('guest');
+Route::post('logout', [SessionController::class, 'destroy'])->middleware('auth');
 
 Route::get('/form', function () {
     return view('form');
